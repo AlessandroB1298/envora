be0c8944d346d5bc5991de36159ade03c95d27ee
AlessandroB1298 <aidanbongiorno09@gmail.com>
2025-03-01 23:14:50 -0500

    some changes for ui

--- app/components/nav/sideNav.tsx
+++ app/components/nav/sideNav.tsx
@@ -7,15 +7,16 @@ const SideNav = () => {
   const [animationsOpen, setAnimationsOpen] = useState<boolean>(false);
   const [textOpen, setTextOpen] = useState<boolean>(false);
   const [formOpen, setFormOpen] = useState<boolean>(false);
+  const [navOpen, setNavOpen] = useState<boolean>(false);
 
   return (
     <div className="h-[100vh] absolute rounded-xl mb-5 pl-5 border-1 border-gray-700 w-[18vw] ">
-      <div>
+      <div className="mt-8">
         <Link
           href={"/"}
-          className="text-5xl mt-3 text-transparent bg-clip-text bg-gradient-to-br from-blue-300 to-blue-700 "
+          className="text-5xl mt-[100px] text-transparent bg-clip-text bg-gradient-to-br from-blue-300 to-blue-700 "
         >
-          EVORA
+          ENVORA
         </Link>
       </div>
       <ul className="flex flex-col gap-3 mt-5 text-white space-y-2 font-medium w-3/4">
@@ -29,21 +30,21 @@ const SideNav = () => {
           Buttons
         </Link>
         {buttonOpen && (
-          <ul className="flex flex-col gap-2 ml-4  w-3/4">
+          <ul className="flex flex-col gap-2 ml-3 w-[12rem]">
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"/dashboard/buttons"}
             >
               Basic
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Medium
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Advanced
@@ -61,21 +62,21 @@ const SideNav = () => {
           Animations
         </Link>
         {animationsOpen && (
-          <ul className="flex flex-col gap-2  ml-4 w-3/4">
+          <ul className="flex flex-col gap-2 ml-3 w-[12rem]">
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Basic
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Medium
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Advanced
@@ -93,21 +94,21 @@ const SideNav = () => {
           Text
         </Link>
         {textOpen && (
-          <ul className="flex flex-col gap-2 ml-4 w-3/4">
+          <ul className="flex flex-col gap-2 ml-3 w-[12rem]">
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Basic
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Medium
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Advanced
@@ -124,21 +125,53 @@ const SideNav = () => {
           Forms
         </Link>
         {formOpen && (
-          <ul className="flex flex-col gap-2 ml-4 w-3/4">
+          <ul className="flex flex-col gap-2 ml-3 w-[12rem]">
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Basic
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Medium
             </Link>
             <Link
-              className="hover:bg-gray-700 bg-gray-800 text-md p-2 rounded-xl"
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
+              href={"#"}
+            >
+              Advanced
+            </Link>
+          </ul>
+        )}
+
+        <Link
+          onClick={() => {
+            setNavOpen(!navOpen);
+          }}
+          className="hover:bg-gray-600 text-md p-2 rounded-xl"
+          href={"#"}
+        >
+          Navs
+        </Link>
+        {navOpen && (
+          <ul className="flex flex-col gap-2 ml-3 w-[12rem]">
+            <Link
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
+              href={"#"}
+            >
+              Basic
+            </Link>
+            <Link
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
+              href={"#"}
+            >
+              Medium
+            </Link>
+            <Link
+              className="hover:bg-gray-600 bg-gray-700 text-md p-2 rounded-xl"
               href={"#"}
             >
               Advanced
--- app/dashboard/buttons/page.tsx
+++ app/dashboard/buttons/page.tsx
@@ -1,19 +1,27 @@
 import { CodePreview } from "@/code-preview";
 import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 export default function Page() {
-  const buttonCode = " <Link className='hover:bg-gray-300'>Click met</Link>";
+  const basicButton =
+    "<Button className=' bg-white text-black hover:bg-gray-300'>Click met</Button>";
+
+  const hoverButton =
+    "<Button className='bg-white text-black hover:bg-gray-300 transform ease-in hover:translate-y-1'>Click met</Button>";
+
+  const roundedButton =
+    "<Button className='bg-white text-black hover:bg-gray-300 rounded-xl shadow-md'>Click met</Button>";
 
   return (
     <div className=" ml-[25vw]  space-y-6 overflow-auto ">
       <section className="grid  mt-10 grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-4 hj">
         <div className="space-y-4">
-          <h2 className="text-xl font-semibold">Button Component</h2>
+          <h2 className="text-xl text-white font-semibold">Basic Button</h2>
           <CodePreview
             className="bg-gray-700 "
-            code={buttonCode}
-            fileName="button-example.tsx"
+            code={basicButton}
+            fileName="buttonBasicExample.tsx"
           >
-            <Button className="hover:bg-gray-300" variant="outline">
+            <Button className="bg-white text-black hover:bg-gray-300">
               Click me
             </Button>
           </CodePreview>
@@ -22,13 +30,13 @@ export default function Page() {
 
       <section className="grid  mt-10 grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-10">
         <div className="space-y-4 justify-between">
-          <h2 className="text-xl font-semibold">Button Component</h2>
+          <h2 className="text-xl text-white font-semibold">Hover Button</h2>
           <CodePreview
             className="bg-gray-700"
-            code={buttonCode}
-            fileName="button-example.tsx"
+            code={hoverButton}
+            fileName="buttonHoverExample.tsx"
           >
-            <Button className="hover:bg-gray-300" variant="outline">
+            <Button className="bg-white text-black hover:bg-gray-300 duration-100 transition ease-in hover:translate-y-2">
               Click me
             </Button>
           </CodePreview>
@@ -37,13 +45,13 @@ export default function Page() {
 
       <section className="grid  mt-10 grid-cols-1 sm:grid-cols-2 lg-grid-cols-4 gap-4">
         <div className="space-y-4">
-          <h2 className="text-xl font-semibold">Button Component</h2>
+          <h2 className="text-xl text-white font-semibold">Rounded</h2>
           <CodePreview
             className="bg-gray-700"
-            code={buttonCode}
-            fileName="button-example.tsx"
+            code={roundedButton}
+            fileName="buttonRoundExample.tsx"
           >
-            <Button className="hover:bg-gray-300" variant="outline">
+            <Button className="bg-white text-black hover:bg-gray-300 rounded-xl shadow-md">
               Click me
             </Button>
           </CodePreview>
